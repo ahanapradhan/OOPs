6b43b3dee2d6237c82826a6f87acb6a37d913a23
Ahana <sendatahana@gmail.com>
2025-07-16 11:26:28 +0530

    Create jerry_likes_cheese.cpp

---
+++ jerry_likes_cheese.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define YUMMY "Yummy"
+
+class Cheese {
+    bool quality;
+    bool isYuck(){
+      return !(this->quality);
+    }
+public:
+    void setData(string quality){
+      this->quality = (quality == YUMMY);
+    }
+    bool isYummy(){
+      return !(this->isYuck());
+    }
+};
+
+class Mouse{
+    string name;
+    int age;
+    bool ILikeIt(Cheese ch){
+      return ch.isYummy();
+    }
+public:
+    setData(string name, int age){
+      this->name = name;
+      this->age = age;
+    }
+    string howDoYouLikeIt(Cheese ch){
+      if (this->ILikeIt(ch)){
+        return "yum yum!";
+      }
+      return "waaakkk!";
+    }
+};
+
+int main(){
+  Mouse jerry;
+  Cheese yellow, limegreen;
+  jerry.setData("jerry", 2);
+  yellow.setData(YUMMY);
+  limegreen.setData("Rubber");
+
+  cout << jerry.howDoYouLikeIt(yellow) << endl;
+  cout << jerry.howDoYouLikeIt(limegreen) << endl;
+}
