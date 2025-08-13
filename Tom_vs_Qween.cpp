6c51d664f42ece8a45f6410c5eb018b845c28b96
Ahana <sendatahana@gmail.com>
2025-08-13 10:59:18 +0530

    Update Tom_vs_Qween.cpp

--- Tom_vs_Qween.cpp
+++ Tom_vs_Qween.cpp
@@ -6,42 +6,53 @@ using namespace std;
 class Qween {
     string name;
     string task;
-    bool taskDone;
+    bool taskDone = false;
+    void shout(const string& taskName) const {
+        cout << "Why do you think I want to work?? Go "<< this->task << " to " << taskName<< " on my behalf.." << endl;
+    }
 public:
-    Qween(string name, string task) {
+    void setDetails(string name, string task) {
         this->name = name;
         this->task = task;
         this->taskDone = false;
     }
-    void performTask(const string& name){
-        cout << "Why do you think I want to work?? Go do "<< this->task << name<< " for me.." << endl;
+    void pleaseOffer(const string& taskName){
+        this->shout(taskName);
+        cout << ":)"<<endl;
         this->taskDone = true;
     }
 };
 
 class FlawlessCat {
-    void assignTaskToQween(Qween& qween, const string& name) {
-        qween.performTask(name);
+    string name;
+    int age;
+    char gender;
+
+    void introduceToQween(Qween& qween, const string& name) {
+        qween.pleaseOffer(name);
     }
 
 public:
-    string name;
-
-    int age;
-
-    char gender;
-    
+    void setDetails(const string& name, int age, char gender) {
+        this->name = name;
+        this->age = age;
+        this->gender = gender;
+    }
     void sayHello(const FlawlessCat&  visitor, Qween& qween) {
-        cout << "Hello " << visitor.name;
-        this->assignTaskToQween(qween, this->name);
+        cout << "Hello " << visitor.name << endl << "My name is " << this->name << endl;
+        cout << "Let me introduce you to the Qween!" <<endl;
+        this->introduceToQween(qween, visitor.name);
     }
 };
 
 
 
 int main() {
-    Qween victoria("Victoria", "Rule");
-    FlawlessCat tom = {"Thomas", 10, 'M'};
-    FlawlessCat toodles = {"Toodles Galore", 2, 'F'};
+    Qween victoria;
+    victoria.setDetails("Victoria", "offer tea");
+    FlawlessCat tom;
+    tom.setDetails("Thomas", 10, 'M');
+    FlawlessCat toodles;
+    toodles.setDetails("Toodles Galore", 2, 'F');
     tom.sayHello(toodles, victoria);
 }
